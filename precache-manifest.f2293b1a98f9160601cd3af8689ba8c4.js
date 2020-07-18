daa7a974bd99fe304edd76fee25b2f49ae242410
Marco Tejeda <jeanmarcotejeda@gmail.com>
2020-07-17 22:22:10 -0500

    Updates

---
+++ precache-manifest.f2293b1a98f9160601cd3af8689ba8c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff42d77ba8125a03bc5412d1123c79a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64748880c07c278e356b",
+    "url": "/static/js/2.d421f5dc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d421f5dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2cbad45cdf265de4b3a",
+    "url": "/static/js/main.0742b79e.chunk.js"
+  },
+  {
+    "revision": "e6ddaa6ca7b319197dad",
+    "url": "/static/js/runtime-main.5816bc3c.js"
+  },
+  {
+    "revision": "7004d4b34ed7f2deabcd3d1df50de999",
+    "url": "/static/media/marcotejeda-logo-dark.7004d4b3.svg"
+  },
+  {
+    "revision": "8f15eb23c0960695157993be68f9ce38",
+    "url": "/static/media/marcotejeda-logo.8f15eb23.svg"
+  }
+]);
